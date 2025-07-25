5ad68fa61b0dc592809bcbd2ec79d21aa1a7e02e
DanishCodebase <ammarsaifi5376@gmail.com>
2025-07-25 17:35:27 +0530

    Update dependencies and integrate Google Tag Manager from @next/third-parties
    - Added @next/third-parties dependency to package.json for enhanced third-party integration.
    - Replaced inline Google Tag Manager script in layout.jsx with a component from @next/third-parties for improved maintainability and readability.
    - Updated pnpm-lock.yaml to reflect the new dependency and its version.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,23 +20,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
+      <head>
         {/* Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-KQX3Q3RF');`,
-          }}
-        />
+        <GoogleTagManager gtmId="GTM-KQX3Q3RF" />
         {/* End Google Tag Manager */}
-      </Head>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {/* Google Tag Manager (noscript) */}
+
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-KQX3Q3RF"
@@ -45,6 +38,7 @@ export default function RootLayout({ children }) {
             style={{ display: "none", visibility: "hidden" }}
           ></iframe>
         </noscript>
+
         {/* End Google Tag Manager (noscript) */}
         {children}
       </body>
